e1189da89fe6a1f3e0c72b9c9e62dfefb1f4d129
Vasiliy0982 <Vasiliysapegin57@gmail.com>
2025-01-31 01:29:46 +0800

    Add files via upload

---
+++ lesson94/main.js
@@ -0,0 +1,6 @@
+const firstName = 'Василий'
+const lastName = 'Косяков'
+let age = 18 
+const hairColor = 'чёрные'
+const eyesColor = 'карие'
+console.log(firstName, lastName, age, 'лет', hairColor, 'волосы', eyesColor, 'глаза')
